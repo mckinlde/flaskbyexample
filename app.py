e8480f0fd0f14a0d1009880ca38da42f14edd3b4
mckinlde <douglas.e.mckinley@vanderbilt.edu>
2020-06-10 02:32:11 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+    
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('namepage.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0') #port :5000 when run from rpi terminal
+
+    
